6de62fcfeb9b276679fd96a3ff16158a9317c2de
akmilia <akmilia@yandex.ru>
2023-04-08 10:15:03 +0300

    OK

--- 8,04 hw.py/1.py
+++ 8,04 hw.py/1.py
@@ -1,1 +1,14 @@
-#написать программу конвертер валют, сделать общение с пользователем через меню
+#написать программу конвертер валют, сделать общение с пользователем через меню 
+print("Hello, Im the currency converter \n Lets get to business! \n Chooose your currency")
+flag = True
+while (flag):
+    #print("1.RUB \n 2.DOL \n 3.EUR \n 4.UAN \n 5.Exit" )
+    a = int(input("1.RUB \n 2.DOL \n 3.EUR \n 4.UAN \n 5.Exit")) 
+    if a==5:
+        break
+    print("Then choose a desired currency")
+    b = int(input("1.RUB \n 2.DOL \n 3.EUR \n 4.UAN \n 5.Exit"))  
+    if b==5:
+        break
+    c = int(input("Enter the sum "))
+
--- 8,04 hw.py/2.py
+++ 8,04 hw.py/2.py
@@ -1,3 +1,22 @@
-# вводится число и границы диаппазона
+# вводится число и границы диаппазона 
+a = int(input("Start"))
+b = int(input("End"))
+c = int(input("Number")) 
+if a>=b:
+    print("Error")
+else: 
+    while True: 
+        c = int(input("Number")) 
+        if c>b or c<a:
+            print("Error")
+        else:
+            False
+for i in range(a, b):
+    if (i!=c):
+        print(i, " ") 
+    else:
+        print("!", i, "! ") 
+
+
 # если число не входит в него, то выводится сообщение об ошибке и просьба ввести заново(  пока не будет правильно)
 # после чего все числа диапазона выводятся, а введеное число выделяется знаками
